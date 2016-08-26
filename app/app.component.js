f43762a4a2060a05f6af8e1ce2f37edd8b5c1241
Maxwell <kmaxwell@seic.com>
2016-08-26 16:03:52 -0400

    styling tweaks, separation of concerns, making css and html for each component

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\">Dashboard</a>\n      <a routerLink=\"/heroes\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  "
+            templateUrl: 'app/app.component.html',
+            styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
